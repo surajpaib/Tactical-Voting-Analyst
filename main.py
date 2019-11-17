93154e5d6d7a2c634872d4ad847d8db79a941ec0
Suraj Pai <surajballambat@gmail.com>
2019-11-17 22:22:11 +0100

    Return voting outcome dict

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from voting_schemes_runner import VotingSchemesRunner
 if __name__ == "__main__":
     preference_creator = PreferenceCreator()
     preference_matrix, selected_scheme = preference_creator.get_preferences()
-    votingrunner = VotingSchemesRunner(preference_matrix, selected_scheme)
+    votingrunner = VotingSchemesRunner()
+    strategic_voting_results = votingrunner.run_voting_simulation(preference_matrix, selected_scheme)
+    print("Strategic Voting Results: {}".format(strategic_voting_results))
--- voting_schemes_runner.py
+++ voting_schemes_runner.py
@@ -1,73 +1,66 @@
 import numpy as np
-
+from collections import OrderedDict
 
 class VotingSchemesRunner:
-    def __init__(self, preference_matrix, voting_scheme):
-        self.preference_matrix = np.array(preference_matrix, dtype=np.uint8)
-        self.voting_scheme = voting_scheme
+    def __init__(self):
+        pass
 
+    def run_voting_simulation(self, preference_matrix, voting_scheme):
+        preference_matrix = np.array(preference_matrix, dtype=np.uint8)
         # Select Voting Schemes to Run
-        if self.voting_scheme == 0:
-            self.plurality_voting(self.preference_matrix)
-        elif self.voting_scheme == 1:
-            self.voting_for_two(self.preference_matrix)
-        elif self.voting_scheme == 2:
-            self.anti_plurality_voting(self.preference_matrix)
-        elif self.voting_scheme == 3:
-            self.borda_voting(self.preference_matrix)    
+        if voting_scheme == 0:
+            return self.plurality_voting(preference_matrix)
+        elif voting_scheme == 1:
+            return self.voting_for_two(preference_matrix)
+        elif voting_scheme == 2:
+            return self.anti_plurality_voting(preference_matrix)
+        elif voting_scheme == 3:
+            return self.borda_voting(preference_matrix)    
 
-    def calculate_voter_happiness(self):
+
+    def calculate_voter_happiness():
         pass
 
     def plurality_voting(self, preference_matrix):
+        preferences = {k:0 for k in preference_matrix[:, 0]}
+
         unique, counts = np.unique(preference_matrix[0, :], return_counts=True)
-        print(unique, counts)
-        self.voting_outcome = unique[np.argmax(counts)]
-        print("\nNon-Strategic Voting Outcome is: {}".format(chr(self.voting_outcome)))
+        for index, element in enumerate(unique):
+            preferences[element] += counts[index]
+
+        return dict(OrderedDict(preferences))
 
     def voting_for_two(self, preference_matrix):
-        preferences = {}
+        preferences = {k:0 for k in preference_matrix[:, 0]}
+
         for i in range(2):
             unique, counts = np.unique(preference_matrix[i, :], return_counts=True)
             for index, element in enumerate(unique):
-                if element not in preferences:
-                    preferences[element] = counts[index]
-                else:
-                    preferences[element] += counts[index]
+                preferences[element] += counts[index]
 
-        
-        self.voting_outcome = max(preferences, key=preferences.get)
-        print("\nNon-Strategic Voting Outcome is: {}".format(chr(self.voting_outcome)))
+        return dict(OrderedDict(preferences))
 
 
     def anti_plurality_voting(self, preference_matrix):
-        preferences = {}
+        preferences = {k:0 for k in preference_matrix[:, 0]}
+
         for i in range(len(preference_matrix[:, 0])):
             unique, counts = np.unique(preference_matrix[i, :], return_counts=True)
             for index, element in enumerate(unique):
-                if element not in preferences:
-                    preferences[element] = counts[index]
-                else:
-                    preferences[element] += counts[index]
+                preferences[element] += counts[index]
 
-        print(preferences)
-        self.voting_outcome = max(preferences, key=preferences.get)
-        print("\nNon-Strategic Voting Outcome is: {}".format(chr(self.voting_outcome)))
+        return dict(OrderedDict(preferences))
 
 
     def borda_voting(self, preference_matrix):
-        preferences = {}
+        preferences = {k:0 for k in preference_matrix[:, 0]}
+
         for i in range(len(preference_matrix[:, 0])):
             borda_factor = len(preference_matrix[:, 0]) - i - 1
             unique, counts = np.unique(preference_matrix[i, :], return_counts=True)
             for index, element in enumerate(unique):
-                if element not in preferences:
-                    preferences[element] = counts[index] * borda_factor
-                else:
-                    preferences[element] += counts[index] * borda_factor
+                preferences[element] += counts[index] * borda_factor
 
-        print(preferences)
-        self.voting_outcome = max(preferences, key=preferences.get)
-        print("\nNon-Strategic Voting Outcome is: {}".format(chr(self.voting_outcome)))
+        return dict(OrderedDict(preferences))
 
         
