f4cdcc302c746ce620370f07b9829c7356b00fd8
Alexander Reisach <alexander.reisach@gmail.com>
2019-11-17 21:43:23 +0100

    added comments to preference_creator

--- preference_creator.py
+++ preference_creator.py
@@ -11,6 +11,7 @@ class PreferenceCreator:
         self.get_number_of_candidates()
         self.get_voting_schemes()
         self.preference_matrix = np.zeros((self.number_of_candidates, self.number_of_voters))
+        # TODO Generalize to unlimited candidates
         self.list_of_candidates = [chr(65 + offset) for offset in range(self.number_of_candidates)]
     
         print("\n Preference Matrix: \n{}".format(self.preference_matrix))
@@ -25,7 +26,7 @@ class PreferenceCreator:
             try: 
                 self.selected_scheme = int(input("Select the Voting Scheme using the indexes \n{} :".format(voting_schemes))) - 1
                 if self.selected_scheme > 3 or self.selected_scheme < 0:
-                    print("\n Select a value between 1 - 4")
+                    print("\n Select a value between 1 and 4")
                     continue
                 else:
                     break
@@ -33,19 +34,20 @@ class PreferenceCreator:
                 print("\nInvalid Scheme selection. Select a value between 1-4")
 
     def get_voter_candidates(self):
+        """Get user input for voter preferences"""
         for voter_number in range(self.number_of_voters):
             print("\n Entering Preferences for Voter {} ".format(voter_number+ 1))
             print("\n Possible options are: {}".format(self.list_of_candidates))
             voter_choices = []
             for candidate_number in range(len(self.list_of_candidates)):
                 while True:
-                    preference = input("Enter the {} Preference Candidate for Voter {}: ".format(candidate_number+1, voter_number+1))
+                    preference = input("Enter the {}. preferred candidate for voter {}: ".format(candidate_number+1, voter_number+1))
                     if (preference in self.list_of_candidates and preference not in voter_choices):
                         voter_choices.append(preference)
                         break
                     else:
                         print("The preference selected is invalid or already chosen. Please choose wisely.")
-            
+            # assign ASCII character values to preferences
             numerical_voter_choices = np.array([int(ord(val)) for val in voter_choices])
             self.preference_matrix[:, voter_number] = numerical_voter_choices
         print("\n Preference Matrix: \n{}".format(self.preference_matrix))
@@ -53,6 +55,7 @@ class PreferenceCreator:
                 
 
     def get_number_of_voters(self):
+        """ask user for number of voters"""
         while True:
             try: 
                 self.number_of_voters = int(input("Enter the number of Voters: "))
@@ -61,6 +64,7 @@ class PreferenceCreator:
                 logging.error("Input value for number of voters is incorrect. Please Enter an Integer value")
 
     def get_number_of_candidates(self):
+        """ask user for number of candidates (up to 26)"""
         while True:
             try: 
                 self.number_of_candidates = int(input("\nEnter the number of Candidates: "))
