d285ec36962b672ee4bf052c4cc86a334ea623b7
zenmar <ammar92_1@hotmail.com>
2019-11-18 01:10:16 +0100

    Compromising Strat

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from preference_creator import PreferenceCreator
 from voting_schemes_runner import VotingSchemesRunner
+from tactical_voting import TacticalVoting
 
 if __name__ == "__main__":
     preference_creator = PreferenceCreator()
     preference_matrix, selected_scheme = preference_creator.get_preferences()
     votingrunner = VotingSchemesRunner()
     strategic_voting_results = votingrunner.run_voting_simulation(preference_matrix, selected_scheme)
+    tactical_voting = TacticalVoting(preference_matrix, strategic_voting_results, selected_scheme)
+    tactical_voting.compromising_strategy(preference_matrix, strategic_voting_results)
     print("Strategic Voting Results: {}".format(strategic_voting_results))      
--- tactical_voting.py
+++ tactical_voting.py
@@ -1,4 +1,7 @@
 from voting_schemes_runner import VotingSchemesRunner
+from itertools import chain, permutations, combinations
+from sympy.utilities.iterables import multiset_permutations
+from copy import copy, deepcopy
 
 class TacticalVoting:
     def __init__(self, preference_matrix, voting_outcome, selected_scheme):
@@ -10,5 +13,20 @@ class TacticalVoting:
     def bullet_voting(self):
         pass
 
-    def compromising_strategy(self):
-        pass
+    def compromising_strategy(self, preference_matrix, voting_outcome):
+        self.vh1 = self.votingrunner.calculate_voters_happiness(preference_matrix, voting_outcome)
+        self.newpref = [[]] * len(preference_matrix[0])
+
+        for i in range(len(preference_matrix[0])):
+            for j in range(len(preference_matrix[:,0])):
+                for k in range(len(preference_matrix[:,0])):
+                    if k != j:
+                        self.pref2 = deepcopy(preference_matrix)
+                        tmp = self.pref2[j,i]
+                        self.pref2[j,i] = self.pref2[k,i]
+                        self.pref2[k,i] = tmp
+                        self.vh2 = self.votingrunner.calculate_voters_happiness(self.pref2, voting_outcome)
+                        #print("\nInit VH : {}, new VH: {}".format(self.vh1,self.vh2))
+                        if (self.vh1[i] < self.vh2[i]):
+                            self.newpref[i] = deepcopy(self.pref2)
+                        # if (calculate happiness>): pref2 = modified_preference_matrix
