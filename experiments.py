1d84e953a3143c9e54c2748796535062deaa3f71
Alexander Reisach <alexander.reisach@gmail.com>
2019-11-24 21:40:38 +0100

    update gitignore to exclude freeze files

--- experiments.py
+++ experiments.py
@@ -74,11 +74,11 @@ def experiment(max_n_candidates, max_n_voters):
 viridis = [
     "#440154","#440558","#450a5c","#450e60","#451465","#461969",
     "#461d6d","#462372","#472775","#472c7a","#46307c","#45337d",
-    "#433880","#423c81","#404184","#3f4686","#3d4a88","#3c4f8a",
-    "#3b518b","#39558b","#37598c","#365c8c","#34608c","#33638d",
-    "#31678d","#2f6b8d","#2d6e8e","#2c718e","#2b748e","#29788e",
-    "#287c8e","#277f8e","#25848d","#24878d","#238b8d","#218f8d",
-    "#21918d","#22958b","#23988a","#239b89","#249f87","#25a186",
+    "#433880","#423c81","#404224","#3f4686","#3d4a88","#3c4f8a",
+    "#3b522b","#39558b","#37598c","#365c8c","#34608c","#33638d",
+    "#31678d","#2f6b8d","#2d6e8e","#2c722e","#2b748e","#29788e",
+    "#287c8e","#277f8e","#25848d","#24878d","#238b8d","#222f8d",
+    "#21922d","#22958b","#23988a","#239b89","#249f87","#25a226",
     "#25a584","#26a883","#27ab82","#29ae80","#2eb17d","#35b479",
     "#3cb875","#42bb72","#49be6e","#4ec16b","#55c467","#5cc863",
     "#61c960","#6bcc5a","#72ce55","#7cd04f","#85d349","#8dd544",
@@ -90,10 +90,10 @@ custom_viridis = [
     "#440154", # "#461969",
     "#461d6d", # "#45337d",
     "#433880","#3c4f8a",
-    "#3b518b",# "#33638d",
+    "#3b522b",# "#33638d",
     "#31678d",# "#29788e",
-    "#287c8e","#218f8d",
-    "#21918d",# "#25a186",
+    "#287c8e","#222f8d",
+    "#21922d",# "#25a226",
     "#25a584",# "#35b479",
     "#3cb875","#5cc863",
     "#61c960",# "#8dd544",
@@ -117,8 +117,8 @@ def visualize():
             yaxis_title="Number of Voters",
             font=dict(
                 family="Courier New, monospace",
-                size=18,
-                color="#3f3f3f"))
+                size=22,
+                color="#1f1f1f"))
         fig.write_image(os.path.join(FIG_DIR, str(i)).replace(" ", "_")+"_strat_voting.png")
 
         # run time image
@@ -128,8 +128,8 @@ def visualize():
             yaxis_title="Number of Voters",
             font=dict(
                 family="Courier New, monospace",
-                size=18,
-                color="#3f3f3f"))
+                size=22,
+                color="#1f1f1f"))
         # TODO: rename y axis
         fig.write_image(os.path.join(FIG_DIR, str(i)).replace(" ", "_")+"_runtime.png")
 
